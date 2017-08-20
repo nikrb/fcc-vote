4892c5884eb817f58ed22564ff9a36aef0ddebf0
nikrb <nik_rb@yahoo.com>
2017-08-20 19:49:16 +0100

    decode params for get my polls and fix /api/poll endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,19 +6,30 @@ const router = new express.Router();
 router.get('/mypolls', (req, res) => {
   let q = {};
   if( req.query.email){
-    q.email = req.query.email;
+    q.email = decodeURIComponent( req.query.email);
   }
   Poll.find( q, function (err, docs){
     res.status(200).json( docs);
   });
 });
 router.post( '/poll', (req, res) => {
-  const np = new Poll( req.body);
-  np.save( (err) => {
-    if( err) {
-      res.json( { success: false, error: err});
+  Poll.find( {owner: req.body.owner, name: req.body.name}, function( err, docs){
+    if( err){
+      console.err( `find poll [${req.body.name}] failed:`, err);
     }
-    res.json( { success: true});
+    let p;
+    if( docs.length){
+      p = docs[0];
+      p.options = req.body.options;
+    } else {
+      p = new Poll( req.body);
+    }
+    p.save( (err) => {
+      if( err) {
+        res.json( { success: false, error: err});
+      }
+      res.json( { success: true});
+    });
   });
 });
 
