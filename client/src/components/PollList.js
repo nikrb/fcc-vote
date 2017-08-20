6f6a60d7133e28c4f5dba1b7bf2dfd2868898d49
nikrb <nik_rb@yahoo.com>
2017-08-20 19:38:03 +0100

    use ListItem in poll option list

--- client/src/components/PollList.js
+++ client/src/components/PollList.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ListItem from './ListItem';
 
 export default class PollList extends React.Component {
+  onPollClick = ( name) => {
+    this.props.onPollClick( name);
+  };
   render = () => {
     const rows = this.props.data.map( (d,i) => {
-      return <li key={i} onClick={this.props.onPollClick} name={d.name} >{d.name}</li>
+      return <ListItem key={i} onItemClick={this.onPollClick} name={d.name} />;
     });
     return (
       <ul>
