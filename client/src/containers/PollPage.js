d82c55fb79ae84191e2c456cef20c57eb876c310
nikrb <nik_rb@yahoo.com>
2017-08-20 19:45:08 +0100

    fill out voting page

--- client/src/containers/PollPage.js
+++ client/src/containers/PollPage.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import PollVoteForm from '../components/PollVoteForm';
+import Actions from './Actions';
+import Auth from '../modules/Auth';
 
 export default class PollPage extends React.Component {
+  state = {
+    poll: { options: []},
+    redirectToHome: false
+  };
+  componentWillMount = () => {
+    const poll_name = decodeURIComponent( this.props.match.params.name);
+    if( poll_name){
+      Actions.getPoll( poll_name)
+      .then( (response) => {
+        this.setState( { poll: response});
+      })
+    } else {
+      this.setState( { redirectToHome: "/"});
+    }
+  };
+  onVote = ( text) => {
+    console.log( "vote option:", text);
+    let email = null;
+    if( Auth.isUserAuthenticated()){
+      email = Auth.getEmail();
+    }
+    Actions.pollVote( this.state.poll.name, text, email)
+    .then( (response) => {
+      console.log( "vote response:", response);
+    });
+  };
   render = () => {
-    const poll = this.props.location.state.poll;
-    console.log( "poll (vote) page:", poll);
+    if( this.state.redirectToHome){
+      return <Redirect to="/" />
+    }
     return (
       <div className="container">
         <h2>Voting Page</h2>
-        <PollVoteForm poll={poll} />
+        <PollVoteForm poll={this.state.poll} onOptionSelect={this.onVote} />
       </div>
     );
   };
